66c7198d97ec822ada810c30c064cdc0c92f8ba5
Dorian MOY <dorian.moy@epitech.eu>
2022-03-02 17:37:40 +0100

    [FIX/ADD] Fixed existing and added new functions

--- src/modules/birthday/provider.ts
+++ src/modules/birthday/provider.ts
@@ -1,15 +1,24 @@
 import { User } from 'discord.js';
-import { getRepository, MoreThan, Repository } from 'typeorm';
+import { getRepository, MoreThan } from 'typeorm';
 import { Birthday } from 'App/database/entities/Birthday';
+import moment from 'moment';
 
-const birthdayRepository: Repository<Birthday> = getRepository(Birthday);
+export function stringToNextBirthday(dateString: string): moment.Moment {
+  let birthday = moment(dateString, 'DD/MM', true).set({ hours: 12 }).local(true);
 
-export async function getUserBirthday(user: User): Promise<Date | undefined> {
-  return (await birthdayRepository.findOne(user.id))?.date;
+  if (birthday.isBefore(moment())) {
+    birthday = birthday.add(1, 'year');
+  }
+  return birthday;
+}
+
+export function getNextBirthdayFromLast(last: Date): Date {
+  return moment(last).add(1, 'year').toDate();
 }
 
 export async function setUserBirthday(user: User, date: Date): Promise<boolean> {
-  if (!(await getUserBirthday(user))) {
+  const birthdayRepository = getRepository(Birthday);
+  if (!(await birthdayRepository.findOne({ userId: user.id }))) {
     await birthdayRepository.insert({ userId: user.id, date });
     return true;
   }
@@ -19,5 +28,13 @@ export async function setUserBirthday(user: User, date: Date): Promise<boolean> 
 }
 
 export async function getNextBirthdays(limit: number = 5): Promise<Birthday[]> {
-  return await birthdayRepository.find({ where: { date: MoreThan(new Date()) }, take: limit });
+  return await getRepository(Birthday).find({ where: { date: MoreThan(new Date()) }, take: limit });
+}
+
+export async function deleteUserBirthday(user: User): Promise<boolean> {
+  return !!(await getRepository(Birthday).delete({ userId: user.id })).affected;
+}
+
+export function getFrenchNextBirthdayRepresentation(date: moment.Moment): string {
+  return date.locale('fr').format('DD MMMM YYYY');
 }
